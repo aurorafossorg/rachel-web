0ca93b57bf248c4148d72ae6f6b5bbaec715e06f
Luís Ferreira <contact@lsferreira.net>
2018-11-11 15:33:33 +0000

    fix requirejs paths
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ var requirejs = ({
 		'jquery.scrollto': {deps: ['jquery'],exports: 'jQuery'}
 	},
 	paths: {
-		'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min',
-		'jquery.ui': 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min',
+		'jquery': 'https://static.aurorafoss.org/aurorafoss/pub/web/jquery/1.8.2/js/jquery.min',
+		'jquery.ui': 'https://static.aurorafoss.org/aurorafoss/pub/web/jqueryui/1.8.23/js/jquery-ui.min',
 		'jquery.scrollto': 'lib/jquery/jquery.scrollTo.min',
-		'underscore': 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.1/underscore-min',
-		'knockout':'http://cdnjs.cloudflare.com/ajax/libs/knockout/2.1.0/knockout-min',
+		'underscore': 'https://static.aurorafoss.org/aurorafoss/pub/web/underscore.js/1.4.1/js/underscore-min',
+		'knockout':'https://static.aurorafoss.org/aurorafoss/pub/web/knockout/2.1.0/js/knockout-min',
 
 		'text': 'lib/require/text',
 		'domready': 'lib/require/domready'
